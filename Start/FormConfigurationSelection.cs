6741f916ce59dfbfde1071013154bf980bf0f90e
Yura <tarachom@gmail.com>
2025-04-19 13:48:07 +0300

    up

--- Start/FormConfigurationSelection.cs
+++ Start/FormConfigurationSelection.cs
@@ -14,33 +14,23 @@ namespace NewProject
 
         public override async ValueTask<bool> OpenProgram(ConfigurationParam? openConfigurationParam)
         {
-            if (await Config.Kernel.DataBase.IfExistsTable(SpecialTables.Constants))
-            {
-                //Запуск фонових задач
-                Config.StartBackgroundTask();
+            //Запуск фонових задач
+            Config.StartBackgroundTask();
 
-                Program.GeneralForm = new FormNewProject() { OpenConfigurationParam = openConfigurationParam };
-                Program.GeneralNotebook = Program.GeneralForm.Notebook;
-                Program.GeneralForm.Show();
+            Program.GeneralForm = new FormNewProject() { OpenConfigurationParam = openConfigurationParam };
+            Program.GeneralNotebook = Program.GeneralForm.Notebook;
+            Program.GeneralForm.Show();
 
-                //Вивід інформації в нижній StatusBar
-                Program.GeneralForm.SetStatusBar();
+            //Вивід інформації в нижній StatusBar
+            Program.GeneralForm.SetStatusBar();
 
-                //Присвоєння користувача
-                Program.GeneralForm.SetCurrentUser();
+            //Присвоєння користувача
+            Program.GeneralForm.SetCurrentUser();
 
-                //Відкрити перші сторінки
-                Program.GeneralForm.OpenFirstPages();
+            //Відкрити перші сторінки
+            Program.GeneralForm.OpenFirstPages();
 
-                return true;
-            }
-            else
-            {
-                Message.Error(this, @"Error: Відсутня таблиця tab_constants. Потрібно відкрити Конфігуратор і зберегти конфігурацію -  
-                    (Меню: Конфігурація/Зберегти конфігурацію - дальше Збереження змін. Крок 1, Збереження змін. Крок 2)");
-
-                return false;
-            }
+            return await ValueTask.FromResult(true);
         }
 
         public override async ValueTask<bool> OpenConfigurator(ConfigurationParam? openConfigurationParam)
